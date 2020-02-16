ee9115e987a5416000200e949df44187b31fb60e
DaveVanStaden <dave.van.staden@outlook.com>
2020-02-16 13:19:31 +0100

    push

---
+++ Assets/Scripts/AI/ZombieMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieMovement : MonoBehaviour
+{
+    NavMeshAgent zombie;
+
+    GameObject target;
+
+    private void Start()
+    {
+        zombie = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Update()
+    {
+        FindTarget();
+    }
+    private void FindTarget()
+    {
+        zombie.SetDestination(target.transform.position);
+    }
+}
